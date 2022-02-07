ffbd2c3787ce9aa978aa9b584655f0cb087008ae
vanaja-artefact <vanaja.penumatsa@artefact.com>
2022-02-07 12:48:09 +0530

    keep_edges_to_target

--- causallearn/utils/PCUtils/SkeletonDiscovery.py
+++ causallearn/utils/PCUtils/SkeletonDiscovery.py
@@ -122,33 +122,17 @@ def skeleton_discovery(data, alpha, indep_test, stable=True, background_knowledg
                 append_value(cg.sepset, y, x, tuple(sepsets))
 
         if show_progress: pbar.refresh()
-        for edge in cg.G.get_graph_edges():
-            f_node = edge.get_node1().get_name()
-            t_node = edge.get_node2().get_name()
-            if (f_node == "X8" or t_node == "X8" ):
-                print("final edges bfr rem",edge)
+
         for (x, y) in list(set(edge_removal)):
             edge1 = cg.G.get_edge(cg.G.nodes[x], cg.G.nodes[y])
             if edge1 is not None:
-                f_node = cg.G.nodes[x].get_name()
-                t_node = cg.G.nodes[y].get_name()
-                if (f_node == "X8" or t_node == "X8" ) and background_knowledge:
-                    print(f_node,"->",t_node,background_knowledge.is_required(edge1.get_node1(), edge1.get_node2()))
                 if background_knowledge:
                     from_to_req = background_knowledge.is_required(edge1.get_node1(), edge1.get_node2())
                     to_from_req = background_knowledge.is_required(edge1.get_node2(), edge1.get_node1())
                     if not (from_to_req or to_from_req):
-                        print(f_node," removing ->",t_node)
                         cg.G.remove_edge(edge1)
                 else:
                     cg.G.remove_edge(edge1)
-        for edge in cg.G.get_graph_edges():
-            f_node = edge.get_node1().get_name()
-            t_node = edge.get_node2().get_name()
-            if (f_node == "X8" or t_node == "X8" ):
-                print("final edges aft rem",edge)
 
     if show_progress: pbar.close()
-    for edge in cg.G.get_graph_edges():
-        print("final edges",edge)
     return cg
